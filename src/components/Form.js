6d2fe095fefa990cd814d5abd7d9876424d2bde4
rohansinha01 <rohansinha01@gmail.com>
2024-01-31 00:10:49 -0500

    state gets updated with gif data

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,36 @@
+import { useState } from "react"
+
 function Form(props){
+    // declare state to track form values
+    const [formData, setFormData] = useState({
+        searchterm: ""
+    })
+
+    // write a handleChange to update the state
+    const handleChange = (event) => {
+        // grab the input
+        const input = event.target
+        // get the text from the input
+        const val = input.value
+        // make a copy of the current state (new object)
+        const newState = {...formData}
+        //update the newState
+        newState.searchterm = val
+        // update the state
+        setFormData(newState)
+    }
+
+    // write a handle form submission
+    const handleSubmit = (event) => {
+        // prevent the refresh from form submission
+        event.preventDefault()
+        // search for movie using our prop
+        props.gifSearch(formData.searchterm)
+    }
     return <div>
-        <form>
-            <input type="text" />
-            <input type="submit" />
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={formData.searchterm} onChange={handleChange}/>
+            <input type="submit" value="Search Gif"/>
         </form>
     </div>
 }
