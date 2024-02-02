dd669dbaf672b92a0d77dceccc81f67617cbbad6
rohansinha01 <rohansinha01@gmail.com>
2024-02-01 21:57:16 -0500

    added Didn't type a search

--- src/components/GifDisplay.js
+++ src/components/GifDisplay.js
@@ -1,16 +1,16 @@
 function GifDisplay(data){
-    
+    console.log(data.gif)
 
     if(data.gif === null){
         return <h1>Please Search for Gif Above</h1>
     }
-
-    if(data.gif.Reponse === "False"){
+    console.log(data.Response)
+    if(data.Response === undefined){
         return <h1>Didn't type a search</h1>
     }
   
   return <div>
-        {console.log(data.gif.data)}
+        {/* {console.log(data.gif.data)} */}
         <h1>{data.gif.data[0].title}</h1>
         <img src={data.gif.data[0].images.fixed_height.url}></img>
         </div>  
